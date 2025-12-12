12988364cfde46acdce59b5aed712425481f57e0
ali27-dev <khanali3229@gmail.com>
2025-12-12 18:03:56 +0500

    Commenting out the App component because of building the StarRating component and i'm also start adding star in the existing component.

---
+++ src/StarRating.js
@@ -0,0 +1,22 @@
+const ratingContainer = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+const ratingStarContainer = { display: "flex", gap: "4px" };
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+export default function StarRating({ maxRating }) {
+  return (
+    <div style={ratingContainer}>
+      <div style={ratingStarContainer}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
