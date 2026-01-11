024a8a3c40c409563f315d8c96981ca43b895144
ali27-dev <khanali3229@gmail.com>
2026-01-11 23:00:26 +0500

    Loading movie details.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -272,13 +273,71 @@ function Movie({ movie, onSelectedMovie }) {
 }
 
 function MovieDetails({ selectedId, onHandleClose }) {
+  const [movie, setMovie] = useState({});
+  const [loding, setLoding] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating: rating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setLoding(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setLoding(false);
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
   return (
     <>
       <div className="details">
-        <button className="btn-back" onClick={onHandleClose}>
-          &larr;
-        </button>
-        {selectedId}
+        {loding ? (
+          <Loding />
+        ) : (
+          <>
+            <header>
+              <button className="btn-back" onClick={onHandleClose}>
+                &larr;
+              </button>
+              <img src={poster} alt={`Poster of ${movie} movie`} />
+              <div className="details-overview">
+                <h2>{title}</h2>
+                <p>
+                  {released}&bull; {runtime}
+                </p>
+                <p>{genre}</p>
+                <p>
+                  <span>⭐️</span>
+                  {rating} IMDb rating
+                </p>
+              </div>
+            </header>
+            <section>
+              <div className="rating">
+                <StarRating maxRating={10} size={24} />
+              </div>
+              <em>{plot}</em>
+              <p>starring {actors}</p>
+              <p>Directed by {director}</p>
+            </section>
+          </>
+        )}
       </div>
     </>
   );
